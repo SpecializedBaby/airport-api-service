f99937b7303cf4f91ea529992bc9a414a29cc16d
Dede <specialized8393@gmail.com>
2024-07-16 15:49:12 +0100

    Installed Django REST Framework and implemented User model in user/models

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "debug_toolbar",
     "user",
     "airport",
@@ -106,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
