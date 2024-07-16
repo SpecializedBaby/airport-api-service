ae402e65c4a5c12be2820d030d39f829a771f1fa
Dede <specialized8393@gmail.com>
2024-07-16 14:47:00 +0100

    Added URLs for user and airport apps

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
+    path("api/airport/", include("airport.urls", namespace="airport")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("admin/", admin.site.urls),
 ] + debug_toolbar_urls()
