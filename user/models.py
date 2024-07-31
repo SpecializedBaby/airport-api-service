7161c13400a4741125f957ae1e22f1f3a9789484
Dede <specialized8393@gmail.com>
2024-07-31 12:41:51 +0100

    Create custom User model from AbstractUser and register to admin

--- user/models.py
+++ user/models.py
@@ -1,6 +1,5 @@
-from django.db import models
-from django.contrib.auth import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.User):
+class User(AbstractUser):
     pass
