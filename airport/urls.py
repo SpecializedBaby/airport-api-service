80ab11f6c5550bf30581e8eb49429fe9fac0e362
Dede <specialized8393@gmail.com>
2024-08-01 11:17:01 +0100

    Add serializers, viewsets, and endpoints for each model
    - Created serializers for each model (e.g., FlightSerializer, AirportSerializer, etc.)
    - Created viewsets for each serializer (e.g., AirportViewSet, FlightViewSet, etc.)
    - Added corresponding endpoints in airport/urls.py

--- airport/urls.py
+++ airport/urls.py
@@ -1,4 +1,20 @@
-urlpatterns = [
-]
+from rest_framework import routers
+from django.urls import path, include
+
+from airport.views import AirportViewSet, RouteViewSet, AirplaneViewSet, FlightViewSet, OrderViewSet, TicketViewSet, \
+    AirplaneTypeViewSet, CrewViewSet
+
+router = routers.DefaultRouter()
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("crews", CrewViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "airport"
