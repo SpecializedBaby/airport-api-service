9860c431339afab80f7f3cfa56b56572b6865e8b
Dede <specialized8393@gmail.com>
2024-08-06 20:45:52 +0100

    Dockerize Django project and configure PostgreSQL database
    - Created Dockerfile to define the Docker image build process.
    - Added docker-compose.yml to manage multi-container Docker applications including PostgreSQL.
    - Configured PostgreSQL database service in Docker Compose with volume support for data persistence.
    - Implemented a custom management command wait_for_db to ensure the database is ready before starting the application.
    - Updated .gitignore to exclude Docker-related files and directories.
    - Created .dockerignore to optimize the Docker build context.
    - Updated settings.py to:
      - Load SECRET_KEY from .env file.
      - Set MEDIA_URL to "/files/media/".
      - Set DEBUG using an environment variable.
      - Configure ALLOWED_HOSTS using an environment variable.
      - Connect to PostgreSQL using dj-database-url.
    - Updated requirements.txt to include necessary packages such as dj-database-url and psycopg2-binary.

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -21,16 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-eq@p-7j(ey)ov7thnc5f)5ee@#e4)lg55prj7vqf0(i*o)p(#$"
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG", default=0))
+
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
 
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -88,8 +89,12 @@ WSGI_APPLICATION = "airport_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
@@ -147,7 +152,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
+MEDIA_URL = "/files/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
